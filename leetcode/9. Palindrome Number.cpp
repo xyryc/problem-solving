7a6b6478e9c0ae4fa5424fcc4fd59e7e5787fc55
xyryc <amdanikzz@gmail.com>
2026-08-13 17:01:54 +0600

    solve: 9. palindrome number

---
+++ leetcode/9. Palindrome Number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+
+        if (x < 0) return false;
+
+        int original = x;
+        long long reverse = 0;
+
+        while (x > 0) {
+            int digit = x % 10;
+            reverse = reverse * 10 + digit;
+            x /= 10;
+        }
+
+        return original == reverse;
+    }
+};
